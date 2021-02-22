4530482129d875a8c20301f493c36c836db841c2
Jinho Hwang <howden2@qraftec.com>
2021-02-22 15:26:23 -0500

    Fixed compilation bug

---
+++ eecs4313a2b/src/eecs4313a2b/TestBlackBox.java
@@ -0,0 +1,14 @@
+package eecs4313a2b;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBlackBox {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ eecs4313a2w/src/eecs4313a2w/TestWhiteBox.java
@@ -0,0 +1,14 @@
+package eecs4313a2w;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestWhiteBox {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
