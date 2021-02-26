c7da356daa9a4d7ba6a3245a7091b6da7796ccda
David Lee <cima123@my.yorku.ca>
2021-02-26 11:12:17 -0500

    Decision Table Testing - V.1

--- assignment2/eecs4313a2b/src/eecs4313a2b/TestBlackBox.java
+++ assignment2/eecs4313a2b/src/eecs4313a2b/TestBlackBox.java
@@ -2,13 +2,38 @@ package eecs4313a2b;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestBlackBox {
 
+	private Date dateAfter, dateBefore, dateBefore2;
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+	
+	@Before
+	public void setUp() throws Exception {
+		String dateString1 = "13-12-2020";
+		String dateString2 = "12-01-2020";
+		dateAfter = sdf.parse(dateString1);
+		dateBefore = sdf.parse(dateString2);
+		dateBefore2 = sdf.parse(dateString2);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsAfterA1() {
+		assertTrue(DateUtil.isAfter(dateAfter, dateBefore));    // A1: Case1, d1 comes after d2
+	}
+	
+	@Test
+	public void testIsAfterA2() {
+		assertFalse(DateUtil.isAfter(dateBefore, dateAfter));   // A2: Case2, d2 comes after d1
+	}
+	
+	@Test
+	public void testIsAfterA3() {
+		assertFalse(DateUtil.isAfter(dateBefore, dateBefore2)); // A3: Case3, two dates are equal
 	}
 
 }
