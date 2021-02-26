feb79622b4e180103261b698ccb3e9f2177defb9
Alman <46537705+AlbertoMastrofrancesco1@users.noreply.github.com>
2021-02-26 11:18:06 -0500

    ect testing commit
    ect testing commit

--- assignment2/eecs4313a2b/src/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
+++ assignment2/eecs4313a2b/src/eecs4313a2b/EECS4313A2AllBlackBoxTests.java
@@ -4,8 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import date_util_minuteStringTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({ TestBlackBox.class })
+//@SuiteClasses({ TestBlackBox.class }) 
+@SuiteClasses ({date_util_minuteStringTest})  //ect
 public class EECS4313A2AllBlackBoxTests {
 
 }
